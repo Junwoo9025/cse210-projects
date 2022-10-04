0950845742ed76812ae5645433d38e57c47ab2ae
Brooklynn Hathaway <hearthebrook@gmail.com>
2022-10-04 13:18:41 -0600

    created deck class

---
+++ Hilow/deck.cs
@@ -0,0 +1,18 @@
+class deck {
+
+    public deck(){
+        // Creates a list to represent our deck
+        // Numbers 1-13 4 times for each suit 
+        
+    }
+    public removeCard(card){
+        // Remove the selected card 
+            
+    }
+
+    public drawRandom(deck) {
+
+    }
+
+
+}
