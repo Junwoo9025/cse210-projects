31dfc58964908b5b5796ebd1901e8b6fccf6ef53
Brooklynn Hathaway <hearthebrook@gmail.com>
2022-10-04 13:39:55 -0600

    worked on deck method

--- Hilow/deck.cs
+++ Hilow/deck.cs
@@ -3,14 +3,22 @@ class deck {
     public deck(){
         // Creates a list to represent our deck
         // Numbers 1-13 4 times for each suit 
+        List<int> cardDeck = new List<int>();
+        for (int x = 1; x<4; x++) {
+            for (int i = 1; i<13; i++) {
+                cardDeck.Add(i);
+            }
+        }
+        return cardDeck;
+
+    }
+    public removeCard(list, card){
+        // Remove the selected card from deckList
         
     }
-    public removeCard(card){
-        // Remove the selected card 
-            
-    }
 
-    public drawRandom(deck) {
+    public drawRandom(list) {
+        // Selects a random card from the deck
 
     }
 
