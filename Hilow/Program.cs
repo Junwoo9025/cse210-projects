587f8060d7877c8ba53464d0fb0f17c34269eaed
TrevorJac14 <trevorjac3@gmail.com>
2022-10-07 14:31:24 -0600

    comments :)

--- Hilow/Program.cs
+++ Hilow/Program.cs
@@ -2,3 +2,11 @@
 
 deck cardDeck = new deck();
 Score scoreCard = new Score();
+
+// Draw a card
+
+// Have player input for higher or lower
+
+// Draw a new card and repeat
+
+// If statement when out of points to start again
