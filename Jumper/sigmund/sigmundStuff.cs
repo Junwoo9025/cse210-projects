dfa0b403afdb70d45bd3755b38ffb25f1966d175
Sigmund1s <sigmundserranilla@gmailcom>
2022-10-13 13:32:17 -0600

    updated

--- Jumper/sigmund/sigmundStuff.cs
+++ Jumper/sigmund/sigmundStuff.cs
@@ -1,2 +1,18 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿/***
+player
+- The player guesses a letter in the puzzle.
+- If the puzzle is solved the game is over.   
+
+word list
+- Creation and holder of list
+- randomly choose a word from the list.
+
+puzzle
+- If the guess is correct, the letter is revealed.
+- If the guess is incorrect, a line is cut on the player's parachute.
+- If the player has no more parachute the game is over.
+
+gamerunner
+- Continue playing
+
+***/
