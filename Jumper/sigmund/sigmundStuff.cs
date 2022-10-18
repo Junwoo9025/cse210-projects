7eb3948e1d01946230e69d4f794729536c41fadf
Sigmund1s <sigmundserranilla@gmailcom>
2022-10-18 13:46:46 -0600

    help

--- Jumper/sigmund/sigmundStuff.cs
+++ Jumper/sigmund/sigmundStuff.cs
@@ -14,6 +14,7 @@ word list
 puzzle
 - (guessCheck) If the guess is correct, the letter is revealed.
 - (guessCheck) If the guess is incorrect, a line is cut on the player's parachute.
+    - To think about: The random word needs to be pulled once. We need to store it.
 - If the player has no more parachute the game is over.
 - If the puzzle is solved the game is over. 
 
@@ -40,20 +41,47 @@ class Word
         // this is the equivalent of "list = []"
     private List<string> listOfWords = new List<string> { "water", "earth", "fire", "air" };
 
+    private string word = "";
+
+    // this void is the setter
+    public void SetRandomWord()
+    {
+        Random random = new Random();
+        int index = random.Next(listOfWords.Count());
+        string randomWord = listOfWords[index];
+        // This is setting the randomly chosen word from the list 
+        word = randomWord;
+        
+        // With a getter and setter. You don't need to return anything.
+        //return randomWord;
+    }
+
+    // this is the getter
+    public string RandomWord()
+    {
+        // this is calling word from the private string
+        return word;
+    }
+
+    /***
 
     public string RandomWord()
     {
         Random random = new Random();
         int index = random.Next(listOfWords.Count());
         string randomWord = listOfWords[index];
+        word = randomWord;
         
-        return randomWord;
+        // With a getter and setter. You don't need to return anything.
+        //return randomWord;
     }
+
+    ***/
 
     
 }
 
-
+/***
 class Puzzle 
 {
     public guessCheck()
@@ -61,12 +89,14 @@ class Puzzle
 
     }
 }
+***/
 
 class GameRunner 
 {
     
     static void Main(string[] args)
     {
+
         Console.Write("What is your guess: ");
         Player choice = new Player();
         string playerchoice = choice.Guess();
@@ -77,6 +107,8 @@ class GameRunner
         // WORKS: This just tests if I'm able to get the random word from the class.
         //***
         Word word = new Word();
+        // The reason to call SetRandomWord so that it can access that list. It is just a void and doesn't return anything.
+        word.SetRandomWord();
         string randomWord = word.RandomWord();
         Console.WriteLine($"The word is: {randomWord}");
         //***/
