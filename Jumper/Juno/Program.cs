e5a7d9afd7d523e96b01e3671b0fb4b72abf27cf
Junwoo Jeone <jgho99025@gmail.com>
2022-10-22 13:26:24 -0600

    Juno

---
+++ Jumper/Juno/Program.cs
@@ -0,0 +1,12 @@
+﻿namespace cse210_03
+{
+    public class Program
+    {
+        //c# will only run Main
+        public static void Main(string[] args)
+        {
+            Director _director = new Director();
+            _director.RunGame();
+        }
+    }
+}
